fix: needs to disable grads for backprop

graph.py
@@ -118,9 +118,9 @@ class TaskGraph(TrainableModel):
     
     def edge(self, src_task, dest_task):
         if isinstance(src_task, ImageTask) and dest_task==task_configs.tasks.LS:
-            return self.edges_out[f"{src_task.name}_down"]
+            return nn.DataParallel(self.edges_out[f"{src_task.name}_down"])
         elif src_task==task_configs.tasks.LS and isinstance(dest_task, ImageTask):
-            return self.edges_in[f"{dest_task.name}_up"]
+            return nn.DataParallel(self.edges_in[f"{dest_task.name}_up"])
         key = str((src_task.name, dest_task.name))
         return self.edge_map[key]            
 
@@ -128,6 +128,7 @@ class TaskGraph(TrainableModel):
         path = [reality] + path
         x = None
         for i in range(1, len(path)):
+            if path[i]==task_configs.tasks.LS: continue
             try:
                 x = cache.get(tuple(path[0:i+1]),
                     self.edge(path[i-1], path[i])(x)
@@ -139,7 +140,9 @@ class TaskGraph(TrainableModel):
                 IPython.embed()
 
             if use_cache: cache[tuple(path[0:(i+1)])] = x
-        if path[-1]==task_configs.tasks.LS: return x[1]
+        if path[-1]==task_configs.tasks.LS:
+            x = self.edge(path[-2], path[-1])(x)
+            return x[1]
         return x
 
     def save(self, weights_file=None, weights_dir=None):

train_ls.py
@@ -58,7 +58,7 @@ def main(
 ):
 
     # CONFIG
-    batch_size = batch_size or (4 if fast else 64)
+    batch_size = batch_size or (4 if fast else 32)
     energy_loss = get_energy_loss(config=loss_config, mode=mode, **kwargs)
 
     # DATA LOADING
@@ -101,28 +101,28 @@ def main(
     logger.add_hook(lambda logger, data: logger.step(), feature="loss", freq=20)
     logger.add_hook(lambda _, __: graph.save(f"{RESULTS_DIR}/graph.pth", RESULTS_DIR_MODELS), feature="epoch", freq=1)
     energy_loss.logger_hooks(logger)
-    energy_loss.plot_paths(graph, logger, realities, prefix="start")
+#     energy_loss.plot_paths(graph, logger, realities, prefix="start")
 
     # BASELINE
-    graph.eval()
-    with torch.no_grad():
-        for _ in range(0, val_step*4):
-            val_loss, _ = energy_loss(graph, realities=[val])
-            val_loss = sum([val_loss[loss_name] for loss_name in val_loss])
-            val.step()
-            logger.update("loss", val_loss)
+#     graph.eval()
+#     with torch.no_grad():
+#         for _ in range(0, val_step*4):
+#             val_loss, _ = energy_loss(graph, realities=[val])
+#             val_loss = sum([val_loss[loss_name] for loss_name in val_loss])
+#             val.step()
+#             logger.update("loss", val_loss)
             
-        for _ in range(0, train_step*4):
-            train_loss, _ = energy_loss(graph, realities=[train])
-            train_loss = sum([train_loss[loss_name] for loss_name in train_loss])
-            train.step()
-            logger.update("loss", train_loss)
-    energy_loss.logger_update(logger)
+#         for _ in range(0, train_step*4):
+#             train_loss, _ = energy_loss(graph, realities=[train])
+#             train_loss = sum([train_loss[loss_name] for loss_name in train_loss])
+#             train.step()
+#             logger.update("loss", train_loss)
+#     energy_loss.logger_update(logger)
 
     # TRAINING
     for epochs in range(0, max_epochs):
         logger.update("epoch", epochs)
-        energy_loss.plot_paths(graph, logger, realities, prefix="")
+#         energy_loss.plot_paths(graph, logger, realities, prefix="")
         
         graph.train()
         for _ in range(0, train_step):